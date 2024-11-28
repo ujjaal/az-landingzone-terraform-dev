c23255909e9afc63390f8f458a1a4e60fc0b475f
ujjaal <ujjaalchanda@gmail.com>
2024-11-29 02:11:06 +0530

    Your commit message here

---
+++ az-landingzone/main.tf
@@ -0,0 +1,40 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used.
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.107"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# You can use the azurerm_client_config data resource to dynamically
+# extract connection settings from the provider configuration.
+
+data "azurerm_client_config" "core" {}
+
+# Call the caf-enterprise-scale module directly from the Terraform Registry
+# pinning to the latest version
+
+module "enterprise_scale" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "<version>" # change this to your desired version, https://www.terraform.io/language/expressions/version-constraints
+
+  default_location = "<YOUR_LOCATION>"
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm
+    azurerm.management   = azurerm
+  }
+
+  root_parent_id = data.azurerm_client_config.core.tenant_id
+  root_id        = "myorg"
+  root_name      = "My Organization"
+}
