a0cf7424d5a67bd249d938dea0b829bbcca3dac6
ujjaal <ujjaalchanda@gmail.com>
2024-11-29 17:05:48 +0530

    Debugging

--- az-landingzone/terraform.tf
+++ az-landingzone/terraform.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = "~> 1.7"
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.18.0"
+      source                = "hashicorp/azurerm"
+      version               = ">= 3.18.0"
       configuration_aliases = [azurerm.connectivity, azurerm.management]
     }
   }
