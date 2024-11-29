cfc05edaf2bd27495db2a9d629302637b2fe865c
ujjaal <ujjaalchanda@gmail.com>
2024-11-29 22:56:34 +0530

    code change

--- az-landingzone/mgmtgrps.tf
+++ az-landingzone/mgmtgrps.tf
@@ -1,8 +1,12 @@
+resource "azurerm_management_group" "root" {
+  name         = "Org Root Mgmt Grp"
+  display_name = data.azurerm_management_group.root.name
+}
 
 resource "azurerm_management_group" "root_landing_zones" {
   name                       = "Org Root Landingzone"
   display_name               = "Org Root Landingzone"
-  parent_management_group_id = data.azurerm_management_group.root.id
+  parent_management_group_id = azurerm_management_group.root.id
 }
 
 resource "azurerm_management_group" "network" {
