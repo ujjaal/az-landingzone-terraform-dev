7a0e0790f069833ca21d03a1ce7fdebb5bd18160
ujjaal <ujjaalchanda@gmail.com>
2024-11-29 18:37:49 +0530

    debug

--- az-landingzone/mgmtgrps.tf
+++ az-landingzone/mgmtgrps.tf
@@ -1,5 +1,5 @@
 resource "azurerm_management_group" "landing_zone" {
   name            = "org-root-landingzone"
   display_name    = "Org Root Landing Zone"
-  parent_group_id = var.parent_group_id
+  parent_management_group_id = var.parent_group_id
 }
