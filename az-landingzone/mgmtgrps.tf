6e679c2848cfb73842a8f8a244999285abc95593
ujjaal <ujjaalchanda@gmail.com>
2024-11-29 18:23:13 +0530

    Formating

--- az-landingzone/mgmtgrps.tf
+++ az-landingzone/mgmtgrps.tf
@@ -1,5 +1,5 @@
 resource "azurerm_management_group" "landing_zone" {
-  name                = "org-root-landingzone"
-  display_name        = "Org Root Landing Zone"
-  parent_group_id     = "/providers/Microsoft.Management/managementGroups/org-root"
+  name            = "org-root-landingzone"
+  display_name    = "Org Root Landing Zone"
+  parent_group_id = "/providers/Microsoft.Management/managementGroups/org-root"
 }
